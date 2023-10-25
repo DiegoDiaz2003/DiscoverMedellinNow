672e6de44b3c2441147c90c13cc9ffec1c71e38f
Lozano0916 <jairoedo2@gmail.com>
2023-10-25 17:37:27 -0500

    Creacion del estilo de las cards

--- src/app/actividades/actividades.component.ts
+++ src/app/actividades/actividades.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiActivity } from '../services/actividades_s/actividades.service';
 
 @Component({
   selector: 'app-actividades',
   templateUrl: './actividades.component.html',
   styleUrls: ['./actividades.component.css']
 })
-export class ActividadesComponent {
+export class ActividadesComponent implements OnInit {
+    datos: any;
+
+    constructor(private apiService: ApiActivity) {}
+
+    ngOnInit() {
+      this.apiService.getDatos().subscribe(data => {
+        this.datos = data;
+      });
+  }
 
 }
