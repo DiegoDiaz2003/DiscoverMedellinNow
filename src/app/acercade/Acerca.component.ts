34cc46a8a8afb39e0bc3dfcf4317af47be2dfcd1
Diego Diaz <diegoalejandrodiazramos@gmail.com>
2023-11-12 19:23:17 -0500

    Acerca de (toca hacer la conexion)

---
+++ src/app/acercade/Acerca.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+    selector: 'app-acercade',
+    templateUrl: "./acercade.html",
+    styleUrls: ['./acercade.css']
+})
+export class AcercadeComponent implements OnInit {
+    data: any;
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      this.http.get('/src/app/componentes/restaurante').subscribe((response) => {
+        this.data = response;
+      });
+    }
+}
