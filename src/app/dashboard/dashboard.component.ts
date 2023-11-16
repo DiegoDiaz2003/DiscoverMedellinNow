efdbd5358cb35739543d38f79787542339ec95d9
Lozano0916 <jairoedo2@gmail.com>
2023-11-16 14:33:44 -0500

    intento eliminar servicio

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,27 +1,59 @@
-import { Component,OnInit } from "@angular/core";
+// dashboard.component.ts
+
+import { Component, OnInit } from "@angular/core";
+import { Observable } from 'rxjs';
 import { ApiActivity } from '../services/actividades_s/actividades.service';
 
-
 @Component({
-    selector: 'app-dashboard ',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-
 export class DashboardComponent implements OnInit {
-    datos: any;
-    eliminarConfirmado: boolean = false; 
+  datos: any;
+//  eliminarConfirmado: boolean = false;
+//  elementoAEliminarId: number | null = null;
 
+  constructor(private apiService: ApiActivity) {}
 
-    constructor(private apiService: ApiActivity) {}
-
-    ngOnInit() {
-      this.apiService.getDatos().subscribe(data => {
-        this.datos = data;
-      });
-  }
-  confirmarEliminar() {
-    console.log('Elemento eliminado');
+  ngOnInit() {
+    this.apiService.getDatos().subscribe(data => {
+      this.datos = data;
+    });
   }
 
+//  eliminarActividad(id: number): Observable<any> {
+//    return this.apiService.eliminarActividad(id);
+//  }
+//
+//  confirmarEliminar(id: number) {
+//    this.eliminarConfirmado = true;
+//    this.elementoAEliminarId = id;
+//  }
+//
+//  realizarEliminacion() {
+//    if (this.elementoAEliminarId !== null) {
+//      this.eliminarActividad(this.elementoAEliminarId).subscribe(
+//        () => {
+//          console.log('Elemento eliminado');
+//          this.actualizarListaDespuesDeEliminacion();
+//        },
+//        (error: any) => console.error('Error al eliminar elemento', error)
+//      );
+//    }
+//
+//    this.eliminarConfirmado = false;
+//    this.elementoAEliminarId = null;
+//  }
+//
+//  cancelarEliminar() {
+//    this.eliminarConfirmado = false;
+//    this.elementoAEliminarId = null;
+//  }
+//
+//  private actualizarListaDespuesDeEliminacion() {
+//    this.apiService.getDatos().subscribe(data => {
+//      this.datos = data;
+//    });
+//  }
 }
